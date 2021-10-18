c1cc3963712fb165c6f707b121fc59fe7ed4c042
OWL又吉 <s-matayoshi@omwonlab.com>
2021-10-18 11:06:27 +0900

    不要なコードを削除

--- packages/external_text/lib/external_text.dart
+++ packages/external_text/lib/external_text.dart
@@ -2,17 +2,8 @@ library external_text;
 
 import 'package:flutter/material.dart';
 
-// import 'package:external_text/poke_text.dart';
-import 'package:external_package/external_package.dart' as test;
 import 'package:test_package/poke_text.dart';
-import 'package:external_text/external_test.dart';
 import 'package:external_text/provider.dart';
-import 'package:test_package/test_package.dart' as tp;
-
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:mock_web_server/mock_web_server.dart';
-import 'package:dio/dio.dart' as Dio;
-
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -24,20 +15,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
       home: ProviderScope(child: _MyHomePageState()),
     );
   }
 }
 
 class _MyHomePageState extends HookWidget {
-  int _counter = 0;
-
-  // final storage = new FlutterSecureStorage();
-  // final server = MockWebServer(port: 9990);
-  // final dio = Dio.Dio();
-  // final headers = <String, String>{'content-type': 'application/json'};
-
   @override
   Widget build(BuildContext context) {
     final externalTest = useProvider(externalTestProvider);
@@ -62,7 +45,6 @@ class _MyHomePageState extends HookWidget {
               '$externalTest',
               style: Theme.of(context).textTheme.headline4,
             ),
-            // tp.TestImage(),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 40.0),
               child: Row(
